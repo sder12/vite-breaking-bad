55dfd3535670519e0c9705956a73a4674aeb18a1
sder12 <sara.cetto@gmail.com>
2022-11-10 14:51:40 +0100

    Start with the layout

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,21 @@
 </script>
 
 <template>
-  <h1>title</h1>
+  <div class="container">
+    <!-- Header + Input -->
+    <header>
+      <h1>Breaking Bad</h1>
+    </header>
+    <!-- Main -->
+    <main class="wrapper">
+      <section>
+        <h4>Select Category</h4>
+      </section>
+      <section>
+        <h3>Found 62 cards</h3>
+      </section>
+    </main>
+  </div>
 </template>
 
 <style lang="scss">
