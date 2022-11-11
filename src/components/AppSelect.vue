427d087ec82092260b94a567ffe1b837f1128731
sder12 <sara.cetto@gmail.com>
2022-11-11 16:24:44 +0100

    Adding select btn

--- src/components/AppSelect.vue
+++ src/components/AppSelect.vue
@@ -6,17 +6,19 @@ export default {
 
 <template>
     <div class="select">
-        <button>
-            Select category
-        </button>
+        <select name="series-tv" id="series-tv">
+            <option value="">Select category</option>
+            <option value="breaking-bad">Breaking Bad</option>
+            <option value="better-call-soul">Better Call Soul</option>
+        </select>
     </div>
 </template>
 
 <style lang="scss" scoped>
 .select {
-    padding: 0em 1em 2em;
+    padding: 0em 1em 1em;
 
-    button {
+    #series-tv {
         padding: .5em 2em;
         border-radius: 5px;
         font-size: 1em;
